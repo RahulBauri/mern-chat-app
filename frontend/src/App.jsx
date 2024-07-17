0fdfa9fae9b1e4f36a5eaaba0e46fbb5434ef4ce
rahul bauri <kumarrahulbauri2000@gmail.com>
2024-07-17 14:17:36 +0530

    2nd part completed - ui design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Login from './pages/login/Login';
+import SignUp from './pages/signup/SignUp';
+import Home from './pages/home/Home';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+      {/* <Login /> */}
+      {/* <SignUp /> */}
+      <Home />
+    </div>
+  );
 };
 
 export default App;
