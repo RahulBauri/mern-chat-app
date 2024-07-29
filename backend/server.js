ee2c267b2b177ec366f7978d95c5fa6eaffe97dc
rahul bauri <kumarrahulbauri2000@gmail.com>
2024-07-29 23:12:05 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -11,6 +12,8 @@ import messageRoutes from './routes/messageRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -27,8 +30,14 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(express.static(path.join(__dirname, './frontend/dist')));
+
 app.get('/test', (req, res) => {
   res.json({ message: 'hello world!' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 server.listen(PORT, () => {
